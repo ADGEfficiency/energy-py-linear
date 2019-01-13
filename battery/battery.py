84eaebcc742be02f408f48a3e2c22cf31708bbb1
Adam Green <adam.green@adgefficiency.com>
2019-01-13 17:39:06 +1300

    battery first draft working

--- battery/battery.py
+++ battery/battery.py
@@ -1,9 +1,7 @@
-
-
 from pulp import LpProblem, LpMinimize, lpSum, LpVariable, LpStatus
 
 
-prices = [10, 20, 30]
+prices = [20, 10, 30, 30]
 idx = range(0, len(prices))
 
 power = 5
@@ -28,7 +26,7 @@ prob += lpSum(
     [b_export[i] * -prices[i] for i in idx[:-1]]
 )
 
-battery_charge[0] == 5.0
+prob += battery_charge[0] == 1.0
 
 for i in idx[:-1]:
     #  energy balance across two time periods
@@ -37,10 +35,6 @@ for i in idx[:-1]:
     #  constrain battery charge level
     prob += battery_charge[i] <= capacity
     prob += battery_charge[i] >= 0
-
-    prob += b_import[i] >= 0
-    prob += b_export[i] >= 0
-
 
 prob.solve()
 print(LpStatus[prob.status])
--- todo.md
+++
@@ -1,3 +0,0 @@
-Battery
-- want the ability to dispatch based on a forecast, then compare with true prices
-- ability to optimize for carbon or price or both
