b9d532b11e1356955e8c2133897f6638ce1dfc34
Thomas Harvey <tom@alush.co.uk>
2019-01-24 18:16:04 +0100

    removig np and pd from the view tests

--- energypylinear/tests/test_battery_view.py
+++ energypylinear/tests/test_battery_view.py
@@ -2,9 +2,6 @@ import pytest
 import io
 import csv
 
-import pandas as pd
-import numpy as np
-
 import energypylinear
 
 
@@ -34,18 +31,15 @@ def test_view_output():
     for row in info:
         writer.writerow(row)
 
-    import pdb;pdb.set_trace()
-    results = pd.DataFrame(info, columns=info[0].keys())
+    csv_rows = csvfile.getvalue().split('\r\n')
     for header in expected_header:
-        assert(header in results.columns)
+        assert(header in csv_rows[0])
 
-    result_rows = results.values.tolist()
-    assert result_rows[0] == [2.0, 0.0, 2.0, 2.0, 0.0, 0.0, 10.0, 10.0, 1.6666666666666667, 1.6666666666666667]
-    assert result_rows[1] == [2.0, 0.0, 2.0, 2.0, 0.0, 0.16666667, 20.0, 20.0, 3.3333333333333335, 3.3333333333333335]
-    assert result_rows[2] == [0.0, 1.6363636, -1.6363636, -1.47272724, 0.16363636, 0.33333333, 30.0, 30.0, -3.6818180999999996, -3.6818180999999996]
-    assert result_rows[3] == [0.0, 2.0, -2.0, -1.8, 0.2, 0.18333333, 40.0, 40.0, -6.0, -6.0]
-    assert result_rows[4] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0, 10.0, 0.0, 0.0]
-    assert np.isnan(result_rows[5]).any()
-    assert result_rows[5][5] == 0.0
-    assert result_rows[5][6] == 50.0
-    assert result_rows[5][7] == 50.0
+    assert csv_rows[1] == ",".join(map(str, [2.0, 0.0, 2.0, 2.0, 0.0, 0.0, 10, 10, 1.6666666666666667, 1.6666666666666667]))
+    assert csv_rows[2] == ",".join(map(str, [2.0, 0.0, 2.0, 2.0, 0.0, 0.16666667, 20, 20, 3.3333333333333335, 3.3333333333333335]))
+    assert csv_rows[3] == ",".join(map(str, [0.0, 1.6363636, -1.6363636, -1.47272724, 0.16363636, 0.33333333, 30, 30, -3.6818180999999996, -3.6818180999999996]))
+    assert csv_rows[4] == ",".join(map(str, [0.0, 2.0, -2.0, -1.8, 0.2, 0.18333333, 40, 40, -6.0, -6.0]))
+    assert csv_rows[5] == ",".join(map(str, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 10, 10, 0.0, 0.0]))
+    assert csv_rows[6].split(",")[5] == "0.0"
+    assert csv_rows[6].split(",")[6] == "50"
+    assert csv_rows[6].split(",")[7] == "50"
