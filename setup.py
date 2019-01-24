cd84e8808205ed62c9815f001aa0315adbaa3567
Thomas Harvey <tom@alush.co.uk>
2019-01-24 18:45:50 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='energypylinear',
 
-    version='0.0.1',
+    version='0.0.2',
     description='linear programming for energy systems',
     author='Adam Green',
     author_email='adam.green@adgefficiency.com',
