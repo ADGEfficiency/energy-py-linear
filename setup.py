ee4778ff26ac2d2cb91688984fae3d6746671c25
Thomas Harvey <tom@alush.co.uk>
2019-01-25 10:36:46 +0100

    including packages

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='energypylinear',
 
-    version='0.0.2',
+    version='0.0.3',
     description='linear programming for energy systems',
     author='Adam Green',
     author_email='adam.green@adgefficiency.com',
     url='http://www.adgefficiency.com/',
 
-    packages=['energypylinear/battery'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
 
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
