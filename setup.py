2442788be3e8fa3bee0e6a4cb07e61cc4204f502
Adam Green <adam.green@adgefficiency.com>
2019-02-09 12:36:34 +1300

    version bump to 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='energypylinear',
-    version='0.0.4',
+    version='0.0.5',
     description='linear programming for energy systems',
     author='Adam Green',
     author_email='adam.green@adgefficiency.com',
