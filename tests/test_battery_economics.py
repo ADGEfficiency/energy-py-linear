6a8522a578157a568076239638eb77cc362a7bb9
Adam Green <adam.green@adgefficiency.com>
2021-09-19 11:34:45 +1200

    master merge

--- tests/test_battery_economics.py
+++ tests/test_battery_economics.py
@@ -1,38 +1,32 @@
 import pytest
 
-import energypylinear
+from energypylinear.battery import Battery, freq_to_timestep
 
 
 @pytest.mark.parametrize(
-    'power, capacity, initial_charge, timestep',
+    "power, capacity, initial_charge, freq",
     [
-        (2, 4, 0, '1hr'),
-        (4, 2, 2, '1hr'),
-        (2, 4, 0, '30min'),
-        (4, 2, 2, '30min'),
-        (2, 4, 0, '5min'),
-        (4, 2, 2, '5min')
-    ]
+        (2, 4, 0, "60T"),
+        (4, 2, 2, "60T"),
+        (2, 4, 0, "30T"),
+        (4, 2, 2, "30T"),
+        (2, 4, 0, "5T"),
+        (4, 2, 2, "5T"),
+    ],
 )
-def test_cost_calculation(power, capacity, initial_charge, timestep):
+def test_cost_calculation(power, capacity, initial_charge, freq):
     prices = [10, 20, 30, -90, 50, 2000, -1]
 
-    model = energypylinear.Battery(
-        power=power, capacity=capacity, efficiency=1.0
+    mdl = Battery(power=power, capacity=capacity, efficiency=1.0)
+    results = mdl.optimize(prices=prices, initial_charge=initial_charge, freq=freq)
+    dispatch = [res["Net [MW]"] for res in results if res["Net [MW]"] is not None]
+
+    timestep = freq_to_timestep(freq)
+    check_actual_costs = (
+        sum([d * p for d, p in zip(dispatch[:-1], prices[:-1])]) / timestep
     )
 
-    info = model.optimize(
-        prices=prices, initial_charge=initial_charge, timestep=timestep
-    )
-
-    dispatch = [res['Net [MW]'] for res in info if res['Net [MW]'] is not None]
-    timestep = model.timestep
-    step = model.step
-    check_actual_costs = sum(
-        [d * p for d, p in zip(dispatch[:-1], prices[:-1])]
-    ) / step
-
-    actual = 'Actual [$/{}]'.format(timestep)
-    actual_costs = [res[actual] for res in info if res[actual] is not None]
+    actual = f"Actual Cost [$/{freq}]"
+    actual_costs = [res[actual] for res in results if res[actual] is not None]
 
     assert round(check_actual_costs, 6) == round(sum(actual_costs[:-1]), 6)
