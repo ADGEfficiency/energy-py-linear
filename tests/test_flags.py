8ecf43be3acf4c49cc51434f850eb04f7783eb10
Adam Green <adam.green@adgefficiency.com>
2023-02-17 18:54:37 +1300

    flag tests

---
+++ tests/test_flags.py
@@ -0,0 +1,21 @@
+"""Test use of boolean flags to change sim and results behaviour."""
+import numpy as np
+
+import energypylinear as epl
+from energypylinear.flags import Flags
+
+
+def test_flags() -> None:
+    """Test that we can correctly use flags in our assets."""
+
+    asset = epl.battery.Battery(
+        power_mw=2.0,
+        capacity_mwh=4.0,
+    )
+    asset.optimize(
+        electricity_prices=np.random.normal(10, 5, 100),
+        freq_mins=5,
+        flags=Flags(include_charge_discharge_binary_variables=True),
+        #  TODO move verbose testing elsewhere
+        verbose=1,
+    )
