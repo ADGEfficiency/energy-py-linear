b36a65ec5895e9ffc458eac3a7e60fe15a0c1ffd
Adam Green <adam.green@adgefficiency.com>
2023-10-17 13:22:20 +1300

    test: test site import & export limits

--- tests/test_site.py
+++ tests/test_site.py
@@ -230,3 +230,42 @@ def test_site_interval_data_export_prices() -> None:
         id = SiteIntervalData(
             electricity_prices=[10, 20, 30], export_electricity_prices=[30, 20, 30, 40]
         )
+<<<<<<< Updated upstream
+=======
+
+
+def test_allow_infeasible() -> None:
+    """Tests the allow infeasible flag."""
+    site = epl.Site(
+        assets=[],
+        electricity_prices=[100, 1000, -20, 40, 50],
+        electric_load_mwh=1000,
+        import_limit_mw=0,
+    )
+    with pytest.raises(AssertionError, match="Infeasible simulation!"):
+        site.optimize(flags=epl.Flags(allow_infeasible=False))
+
+    site.optimize(flags=epl.Flags(allow_infeasible=True))
+
+
+@pytest.mark.parametrize("import_limit_mw", [0, 10, 20, 30])
+@pytest.mark.parametrize("export_limit_mw", [0, 5, 10, 15])
+def test_import_export_limits(import_limit_mw: float, export_limit_mw: float) -> None:
+    """Test the site import & export limits."""
+    site = epl.Site(
+        assets=[
+            epl.Battery(
+                power_mw=120, capacity_mwh=120, efficiency_pct=1.0, name="big-battery"
+            ),
+        ],
+        electricity_prices=[-1000, 1000, -1000, 1000, -1000, 1000],
+        import_limit_mw=import_limit_mw,
+        export_limit_mw=export_limit_mw,
+        freq_mins=60,
+    )
+
+    simulation = site.optimize()
+    assert simulation.feasible
+    assert simulation.results["site-import_power_mwh"].max() <= import_limit_mw
+    assert simulation.results["site-export_power_mwh"].max() <= export_limit_mw
+>>>>>>> Stashed changes
