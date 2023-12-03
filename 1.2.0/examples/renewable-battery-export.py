b8f51ea75dd43b91da0237706d02aae4cdbd1cff
Adam Green <adam.green@adgefficiency.com>
2023-12-03 19:10:31 +1200

    Deployed 5cf24e2 to 1.2.0 with MkDocs 1.5.3 and mike 1.1.2

---
+++ 1.2.0/examples/renewable-battery-export.py
@@ -0,0 +1,19 @@
+"""Example of a site with a renewable generator and a battery exporting to the grid."""
+import energypylinear as epl
+
+assets = [
+    epl.Battery(power_mw=10, capacity_mwh=20, efficiency_pct=0.9),
+    epl.RenewableGenerator(
+        electric_generation_mwh=[10, 20, 30, 20, 10],
+        electric_generation_lower_bound_pct=0.5,
+        name="solar",
+    ),
+]
+
+site = epl.Site(
+    assets=assets,
+    electricity_carbon_intensities=[0.5, -0.5, 0.5, 0.5, -0.5],
+    export_limit_mw=25,
+)
+
+simulation = site.optimize(objective="carbon")
